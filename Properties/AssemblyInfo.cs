0187329ee78f2e1e0abdea8e4a279d41f620020c
jimmyguo <zz90051523@gmail.com>
2026-01-14 11:33:00 +0800

    fix: update workflow and bump version to 1.0.0.7

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      組建編號
 //      修訂編號
 // 0114 1.0.0.1
-[assembly: AssemblyVersion("1.0.0.6")]
-[assembly: AssemblyFileVersion("1.0.0.6")]
+[assembly: AssemblyVersion("1.0.0.7")]
+[assembly: AssemblyFileVersion("1.0.0.7")]
